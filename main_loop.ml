0b8d1f78073e7fe87a3ded097b42aef70f6fbd43
Ambre Gauci-Dalle <agauci-d@student.42.fr>
2016-06-25 09:57:07 +0200

    main_loop wip

--- main_loop.ml
+++ main_loop.ml
@@ -16,6 +16,44 @@
 
 *)
 
+let play_new_game () =
+	let creature_bis = create_creature () in
+	main_loop creature (creature.is_alive ()) 
+
+let do_action action creature =
+	match action with
+	| 0 -> creature.eat ()
+	| 1 -> creature.bath ()
+	| 2 -> creature.thunder ()
+	| 3 -> creature.kill ()
+	| 4 -> save_and_quit creature
 
 
-let main_loop 
+let rec main_loop creature creature_state =
+	match creature_state with 
+	| false -> 
+			begin
+				Display.display_gameover ()
+				Display.display_new_game ()
+				let input = Display.get_input () in 
+				if (input = 5)
+					then play_new_game ()
+				else ()
+			end
+	| true	-> 
+			begin 
+				Display.clear_win ()
+				Display.display_creature creature
+				Display.display_meters creature.get_meters
+				Display.display_actions creature.actions 
+				Display.display_button_save_and_quit ()
+				let player_action = Display.get_event ()
+				in let new_creature = do_action player_action creature
+				in main_loop new_creature (new_creature.is_alive ()) 
+			end
+
+
+let () =
+	Display.create_win
+	let creature = create_creature () in 
+		main_loop creature (creature.is_alive ()) 
